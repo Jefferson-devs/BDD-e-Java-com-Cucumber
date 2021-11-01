4421205c3b027280f493374b5af9bc1ec81e7969
Jefferson-devs <jeffersonsoares.s@yahoo.com>
2021-11-01 17:54:04 -0300

    Cucumber

--- src/test/java/br/com/alura/leilao/acceptance/LeilaoCucumberRunner.java
+++ src/test/java/br/com/alura/leilao/acceptance/LeilaoCucumberRunner.java
@@ -5,8 +5,7 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
+
 @RunWith(Cucumber.class) 
 @CucumberOptions(features = "classpath:features")
-public class LeilaoCucumberRunner {
-
-}
+public class LeilaoCucumberRunner {}
